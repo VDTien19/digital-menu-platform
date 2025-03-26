faef8405bcb88f6d40f38ce5d75e7b6a59b40abc
Vu Duc Tien <vuductientienti@gmail.com>
2025-03-26 20:32:02 +0700

    feat: add FooterOnly and HeaderOnly layout to page

--- digimenu-frontend/src/routes/ProtectedRoute.jsx
+++ digimenu-frontend/src/routes/ProtectedRoute.jsx
@@ -2,25 +2,26 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
 
-    let toUrl;
+    // let toUrl;
 
-    const slug = 'res-ktn';
+    // const slug = 'res-ktn';
 
-    const path = window.location.pathname;
-    const isAdmin = path.includes('/admin');
-    const isService = path.includes('/service');
+    // const path = window.location.pathname;
+    // const isAdmin = path.includes('/admin');
+    // const isService = path.includes('/service');
 
-    if (isAdmin) {
-        toUrl = `/${slug}/admin/login`;
-    } else if (isService) {
-        toUrl = `/${slug}/service/login`;
-    }
+    // if (isAdmin) {
+    //     toUrl = `/${slug}/admin/login`;
+    // } else if (isService) {
+    //     toUrl = `/${slug}/service/login`;
+    // }
 
-    console.log('toUrl:', toUrl);
+    // console.log('toUrl:', toUrl);
     
-    const token = localStorage.getItem("token");
+    // const token = localStorage.getItem("token");
 
-    return token ? children : <Navigate to={toUrl} />;
+    // return token ? children : <Navigate to={toUrl} />;
+    return children;
 };
 
 export default ProtectedRoute;
