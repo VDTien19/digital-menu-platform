a8c7742f91444502c1e4fa619f21b6937f7344ed
Vu Duc Tien <vuductientienti@gmail.com>
2025-03-29 15:38:13 +0700

    used SlugContext

--- digimenu-frontend/src/main.jsx
+++ digimenu-frontend/src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import './index.css';
 import App from './App.jsx';
 import GlobalStyles from '~/components/GlobalStyles';
+import { CategoryProvider } from '~/contexts/CategoryContext';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <GlobalStyles>
-            <App />
+            <CategoryProvider>
+                <App />
+            </CategoryProvider>
         </GlobalStyles>
     </StrictMode>,
 );
