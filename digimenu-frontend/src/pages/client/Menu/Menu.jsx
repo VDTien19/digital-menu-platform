30346693fed963a8f345faf6f6bce8442651169a
Vu Duc Tien <vuductientienti@gmail.com>
2025-03-29 15:41:37 +0700

    feat: connect fake api to render data in UI

--- digimenu-frontend/src/pages/client/Menu/Menu.jsx
+++ digimenu-frontend/src/pages/client/Menu/Menu.jsx
@@ -1,14 +1,28 @@
-import {  } from 'react';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
+import axios from 'axios';
 
 import styles from './Menu.module.scss';
+import CategoryList from '~/components/CategoryList';
 
 const cx = classNames.bind(styles);
 
+
 function Menu () {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://172.20.10.3:8080/categories')
+            .then(res => {
+                // console.log(res.data);
+                setData(res.data);
+            })
+            .catch(err => console.error(err));
+    }, [])
+
     return (
         <div className={cx('wrapper')}>
-            <h1>Menu Client Page</h1>
+            <CategoryList categories={data} />
         </div>
     );
 }
