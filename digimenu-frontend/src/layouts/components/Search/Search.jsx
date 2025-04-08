1ce40b93ee207ea569031cae2c0f77125f4e69b4
Vu Duc Tien <vuductientienti@gmail.com>
2025-04-08 23:03:03 +0700

    feat: used loading icon for search invoice

--- digimenu-frontend/src/layouts/components/Search/Search.jsx
+++ digimenu-frontend/src/layouts/components/Search/Search.jsx
@@ -3,49 +3,90 @@ import { useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 import styles from './Search.module.scss';
-import { SearchIcon, CloseCircleIcon } from '~/components/Icons';
+import { SearchIcon, CloseCircleIcon, LoadingIcon } from '~/components/Icons';
 import { useSearch } from '~/contexts/SearchContext';
 import { useDebounce } from '~/hooks';
+import  * as httpRequest from '~/utils/httpRequest';
 
 const cx = classNames.bind(styles);
 
 function Search() {
     const [isSearch, setIsSearch] = useState(false);
-    const { searchValue, setSearchValue } = useSearch();
+    const [loading, setLoading] = useState(false);
+
+    const { searchValue, setSearchValue, setInvoices } = useSearch();
 
     const inputRef = useRef(null);
 
     const location = useLocation();
-    const isMenuPage = location.pathname.includes("/menu/");
-    const isInvoicePage = location.pathname.includes("/invoice");
+    const isMenuPage = location.pathname.includes('/menu/');
+    const isInvoicePage = location.pathname.includes('/invoice');
 
-    const debouncedSearchValue = useDebounce(searchValue, 1000);
+    const debouncedSearchValue = useDebounce(searchValue, 600);
 
     useEffect(() => {
-        if (isInvoicePage) {
-            // thực hiện search logic ở đây bằng debouncedSearchValue
-            console.log("Debounced search for invoice:", debouncedSearchValue);
+        if (isInvoicePage && searchValue.trim() !== '') {
+            setLoading(true);
+        } else {
+            setLoading(false);
         }
+    }, [searchValue, isInvoicePage]);
+
+    useEffect(() => {
+        const fetchInvoices = async () => {
+            if (isInvoicePage && debouncedSearchValue.trim() !== '') {
+                setLoading(true);
+                try {
+                    setLoading(true);
+                    const response = await httpRequest.get(`invoices?customer_phone=${debouncedSearchValue}`);
+                    setInvoices(response);
+                    console.log("Invoices:", response);
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    setLoading(false);
+                }
+            }
+        };
+        fetchInvoices();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [debouncedSearchValue, isInvoicePage]);
+
+    // useEffect(() => {
+    //     console.log("loading state changed:", loading);
+    // }, [loading]);
 
     const handleChange = (e) => {
         const searchValue = e.target.value;
 
-        if(!searchValue.startsWith(' ')) {
+        if (!searchValue.startsWith(' ')) {
             setSearchValue(e.target.value);
             setIsSearch(true);
         }
-        
-        if(searchValue === '') {
+
+        if (searchValue === '') {
             setIsSearch(false);
         }
-
     };
 
     const handleClear = () => {
         setSearchValue('');
         setIsSearch(false);
         inputRef.current.focus();
+    };
+
+    const handleKeyDown = (e) => {
+        if (isInvoicePage) {
+            if (
+                e.key === 'e' ||
+                e.key === 'E' ||
+                e.key === '+' ||
+                e.key === '-' ||
+                e.key === '.'
+            ) {
+                e.preventDefault();
+            }
+        }
     };
 
     return (
@@ -59,12 +100,24 @@ function Search() {
                     value={searchValue}
                     onChange={handleChange}
                     className={cx('input')}
-                    type="text"
-                    placeholder={isMenuPage ? "Bạn muốn tìm món gì ?" : "Nhập số điện thoại"}
+                    placeholder={
+                        isMenuPage
+                            ? 'Bạn muốn tìm món gì ?'
+                            : 'Nhập số điện thoại'
+                    }
+                    type={isInvoicePage ? 'number' : 'text'}
+                    inputMode={isInvoicePage ? 'numeric' : ''}
+                    pattern={isInvoicePage ? '[0-9]*' : ''}
+                    autoComplete={isInvoicePage ? 'off' : ''}
+                    onKeyDown={handleKeyDown}
                 />
                 {isSearch && (
-                    <div className={cx('close-btn')} onClick={handleClear}>
-                        <CloseCircleIcon />
+                    <div className={cx('icon-btn')}>
+                        {loading ? (
+                            <div className={cx('loading-btn')}><LoadingIcon /></div>
+                        ) : (
+                            <div className={cx('close-btn')} onClick={handleClear}><CloseCircleIcon /></div>
+                        )}
                     </div>
                 )}
             </div>
