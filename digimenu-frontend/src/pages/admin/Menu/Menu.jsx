18720c913c79ded72362df8986f77ee2a4039f25
Vu Duc Tien <vuductientienti@gmail.com>
2025-04-15 23:52:24 +0700

    feat: add AdminContentHeader with all page of admin

--- digimenu-frontend/src/pages/admin/Menu/Menu.jsx
+++ digimenu-frontend/src/pages/admin/Menu/Menu.jsx
@@ -1,14 +1,20 @@
 import {  } from 'react';
 import classNames from 'classnames/bind';
+import { useSearchParams } from 'react-router-dom';
 
 import styles from './Menu.module.scss';
+import AdminContentHeader from '~/components/admin/AdminContentHeader';
 
 const cx = classNames.bind(styles);
 
 function Menu () {
+    const [searchParams] = useSearchParams();
+    const name = searchParams.get('name');
+    
     return (
         <div className={cx('wrapper')}>
-            <h1>Menu Admin Page</h1>
+            <AdminContentHeader title="Quản lý sản phẩm" titleBtn="Thêm mới" onClick={() => console.log('Click button')} />
+            <div>{name}</div>
         </div>
     );
 }
--- digimenu-frontend/src/pages/admin/Table/Table.jsx
+++ digimenu-frontend/src/pages/admin/Table/Table.jsx
@@ -2,13 +2,14 @@ import {  } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Table.module.scss';
+import AdminContentHeader from '~/components/admin/AdminContentHeader';
 
 const cx = classNames.bind(styles);
 
 function Table () {
     return (
         <div className={cx('wrapper')}>
-            <h1>Table Admin Page</h1>
+            <AdminContentHeader title="Quản lý bàn" titleBtn="Thêm mới" onClick={() => console.log('Click button')} />
         </div>
     );
 }
