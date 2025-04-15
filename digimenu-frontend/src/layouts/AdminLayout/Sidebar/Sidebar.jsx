2044d45dc8049f9d6ba749acb91fb80d9809f68c
Vu Duc Tien <vuductientienti@gmail.com>
2025-04-15 20:39:15 +0700

    update: change icon in sidebar

--- digimenu-frontend/src/layouts/AdminLayout/Sidebar/Sidebar.jsx
+++ digimenu-frontend/src/layouts/AdminLayout/Sidebar/Sidebar.jsx
@@ -1,11 +1,12 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faInfo, faChartBar, faUtensils, faTableList, faPeopleGroup } from '@fortawesome/free-solid-svg-icons'
 
 import config from '~/config';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu';
 import { useSlug } from '~/contexts/SlugContext';
+import Image from '~/components/Images';
 
 const cx = classNames.bind(styles);
 
@@ -13,7 +14,7 @@ const MENU_ITEMS = [
     {
         title: 'Dashboard',
         to: config.routes.admin_dashboard,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faHouse} />
         // icon: {
         //     active: <HomeActiveIcon />,
         //     default: <HomeIcon />,
@@ -22,28 +23,33 @@ const MENU_ITEMS = [
     {
         title: 'Giới thiệu',
         to: config.routes.admin_introduce,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faInfo} />
     },
     {
-        title: 'Quản lý loại sản phẩm',
+        title: 'Quản lý loại danh mục',
         to: config.routes.admin_category,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faChartBar} />
     },
     {
         title: 'Quản lý sản phẩm',
         to: config.routes.admin_menu,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faUtensils} />
     }, 
     {
         title: 'Quản lý bàn',
         to: config.routes.admin_table,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faTableList} />
     },
     {
-        title: 'Quản lý nhân viên',
+        title: 'Quản lý nhân sự',
         to: config.routes.admin_staff,
-        icon: <FontAwesomeIcon icon={faXmark} />
+        icon: <FontAwesomeIcon icon={faPeopleGroup} />
     }, 
+    // {
+    //     title: 'Quản lý hoá đơn',
+    //     to: config.routes.admin_invoice,
+    //     icon: <FontAwesomeIcon icon={faPeopleGroup} />
+    // }, 
 ];
 
 function Sidebar() {
@@ -51,7 +57,19 @@ function Sidebar() {
     const { slug } = useSlug();
 
     return (
-        <aside className={cx('wrapper')}>
+        <aside className={cx('wrapper', 'bg-white')}>
+            {/* <div className="flex justify-end mb-4 lg:hidden">
+                <button onClick={closeSidebar} className="text-gray-600 text-2xl font-bold">
+                    ✕
+                </button>
+            </div> */}
+
+            <div className={cx('sidebar-header', 'flex', 'flex-col', 'items-center', 'justify-center', 'mb-8')} onClick={(e) => e.stopPropagation()}>
+                <Image src='a' alt="Logo" className={cx('image-url', 'w-50', 'h-50', 'rounded-full', 'object-cover')} />
+                <p className={cx('text-3xl', 'mt-4', 'font-medium', '')}>Admin</p>
+                <p className={cx('text-xl', 'text-gray-500')}>@slug</p>
+            </div>
+
             <Menu>
                 {MENU_ITEMS.map((item, index) => (
                     <MenuItem
