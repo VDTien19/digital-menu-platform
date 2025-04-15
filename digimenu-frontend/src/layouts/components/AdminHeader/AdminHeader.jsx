3c587a01df1b25fc30a600884caeca1678bd29cd
Vu Duc Tien <vuductientienti@gmail.com>
2025-04-15 20:40:56 +0700

    feat: create AdminHeader component

---
+++ digimenu-frontend/src/layouts/components/AdminHeader/AdminHeader.jsx
@@ -0,0 +1,38 @@
+import {  } from 'react';
+// import { useLocation } from 'react-router-dom';
+import classNames from 'classnames/bind';
+
+import styles from './AdminHeader.module.scss';
+import { BarIcon, SettingIcon } from '~/components/Icons';
+import AdminSearch from '../AdminSearch';
+
+const cx = classNames.bind(styles);
+
+function AdminHeader ({ onToggleSidebar }) {
+    // const location = useLocation();
+    // const isCategory = location.pathname.includes('/admin/category');
+    // const isMenu = location.pathname.includes('/admin/menu');
+    // const isTable = location.pathname.includes('/admin/table');
+    // const isStaff = location.pathname.includes('/admin/staff');
+
+    return (
+        <div className={cx('wrapper', 'w-full', 'h-24', 'bg-transparent', 'flex', 'items-center', 'justify-between')}>
+            <button className={cx('lg:hidden', 'cursor-pointer')} onClick={onToggleSidebar}>
+                <BarIcon width='2.4rem' height='2.4rem' className={cx('bar-icon', 'rotate-90')} />
+            </button>
+            {/* {(isCategory || isMenu || isTable || isStaff) && (
+                <div className={cx('search', 'flex-1', 'mx-8', 'lg:ml-0')}>
+                    <AdminSearch />
+                </div>
+            )} */}
+            <div className={cx('search', 'flex-1', 'mx-8', 'lg:ml-0')}>
+                <AdminSearch />
+            </div>
+            <button className={cx('setting-icon', 'cursor-pointer', 'lg:hidden')} >
+                <SettingIcon />
+            </button>
+        </div>
+    );
+}
+
+export default AdminHeader;
