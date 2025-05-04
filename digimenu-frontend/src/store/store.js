d56df88a9aaa7a09e2feacde2e46dc05532445c5
Vu Duc Tien <vuductientienti@gmail.com>
2025-05-04 19:55:02 +0700

    feat: create tableSlice to management Table state

--- digimenu-frontend/src/store/store.js
+++ digimenu-frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 import categoryReducer from './categorySlice';
 import productReducer from './productSlice';
+import tableReducer from './tableSlice';
 
 export const store = configureStore({
     reducer: {
         cart: cartReducer,
         category: categoryReducer,
         product: productReducer,
+        table: tableReducer,
     }
 });
