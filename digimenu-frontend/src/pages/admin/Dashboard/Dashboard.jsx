e8adfffc7907dad675f037e3e42cfa6bde596d86
Vu Duc Tien <vuductientienti@gmail.com>
2025-05-15 23:19:16 +0700

    update: Integrate DashboardStatic and RevenueComparisonChart into Dashboard page

--- digimenu-frontend/src/pages/admin/Dashboard/Dashboard.jsx
+++ digimenu-frontend/src/pages/admin/Dashboard/Dashboard.jsx
@@ -1,14 +1,38 @@
-import {  } from 'react';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Dashboard.module.scss';
+import DashboardStatic from '~/components/admin/DashboardStatic';
+import RevenueComparisonChart from '~/components/admin/RevenueComparisonChart';
+import RevenueChart from '~/components/admin/RevenueChart';
+import DateFilter from '~/components/admin/DateFilter';
+import TopProductsChart from '~/components/admin/TopProductsChart';
+import {
+    useGetRevenueQuery,
+    useGetTopProductsQuery,
+} from '~/store/dashboardSlice';
 
 const cx = classNames.bind(styles);
 
-function Dashboard () {
+function Dashboard() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('http://172.20.10.3:8080/revenueComparison')
+            .then((res) => res.json())
+            .then((json) => {
+                setData(json);
+            });
+    }, []);
+
     return (
-        <div className={cx('wrapper')}>
-            <h1>Dashboard Admin Page</h1>
+        <div className={cx('wrapper', 'p-3')}>
+            <div className={cx('overview', 'mb-8')}>
+                <DashboardStatic />
+            </div>
+            <div>
+                <RevenueComparisonChart data={data} />
+            </div>
         </div>
     );
 }
