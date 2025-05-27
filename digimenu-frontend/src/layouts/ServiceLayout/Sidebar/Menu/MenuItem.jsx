f9837cd5778513180169f903c8f07f6afc5fa87d
Vu Duc Tien <vuductientienti@gmail.com>
2025-05-28 00:09:47 +0700

    feat: create Sidebar for service layout

---
+++ digimenu-frontend/src/layouts/ServiceLayout/Sidebar/Menu/MenuItem.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames/bind';
+import styles from './Menu.module.scss';
+
+const cx = classNames.bind(styles);
+
+function MenuItem({ title, to, icon }) {
+    return (
+        <NavLink
+            to={to}
+            end // Chỉ active nếu URL khớp chính xác
+            className={(navData) => cx('menu-item', { active: navData.isActive })}
+        >
+            {/* isActive là một giá trị do React Router cung cấp thông qua callback */}
+                <div className={cx('flex', 'items-center')}>
+                    <div className={cx('icon', 'w-8', 'h-8', 'flex', 'items-center', 'justify-center')}>
+                        {icon}
+                    </div>
+                    <span className={cx('title')}>{title}</span>
+                </div>
+        </NavLink>
+    );
+}
+
+MenuItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    to: PropTypes.string.isRequired,
+    icon: PropTypes.shape({
+        active: PropTypes.node.isRequired,
+        default: PropTypes.node.isRequired,
+    }).isRequired,
+};
+
+export default MenuItem;
