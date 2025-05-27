f2ae819516cedcee2c1fe573a3af64b9f5c559b0
Vu Duc Tien <vuductientienti@gmail.com>
2025-05-28 00:13:11 +0700

    feat: create Home page of service

--- digimenu-frontend/src/pages/service/Home/Home.jsx
+++ digimenu-frontend/src/pages/service/Home/Home.jsx
@@ -2,13 +2,24 @@ import {  } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './Home.module.scss';
+import DashboardStatic from '~/components/admin/DashboardStatic';
+import RevenueChart from '~/components/admin/RevenueChart';
+import TopProductsChart from '~/components/admin/TopProductsChart';
 
 const cx = classNames.bind(styles);
 
 function Home () {
     return (
         <div className={cx('wrapper')}>
-            <h1>Home Service Page</h1>
+            <div className={cx('mb-8')}><DashboardStatic /></div>
+            <div className={cx('chart', 'gap-8')}>
+                <div className={cx('revenue-chart', 'mb-8')}>
+                    <RevenueChart />
+                </div>
+                <div className={cx('product-chart')}>
+                    <TopProductsChart />
+                </div>
+            </div>
         </div>
     );
 }
